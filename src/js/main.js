9c2486cd094745c9875595b21db3c8483b13103a
@vdmitriyv88 <ctifsanders@hotmail.com>
2019-09-09 20:33:20 +0500

    composition

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,54 @@
-import Counter   from './components/Counter'
-import {BaseButton, PrettyButton, FunctionalButton} from './components/Buttons'
+import Domworker from './DomWorker'
 
-const baseBtn = new BaseButton('footer', 'base')
-baseBtn.render()
+import TitleComponent from './components/TitleComponent'
+import ListComponent from './components/ListComponent'
+import CenterComponent from './components/CenterComponent'
 
-const prettyBtn = new PrettyButton('footer', 'pretty', 'pretty-btn')
-prettyBtn.render()
+const names = ['Harry', 'Rohn', 'Jiny', 'Germiona']
+Domworker.mount(new TitleComponent('Pro'), 
+document.getElementById('title'))
 
-const funcBtn = new FunctionalButton(
-    'footer',
-    'best',
-    'pretty-btn',
-    ()=> {alert('fungfdjhgdfjhg')} 
-)   
-funcBtn.render()
+Domworker.mount(new ListComponent(names), 
+document.getElementById('list')
+)
+
+Domworker.mount(
+    new CenterComponent(
+        new ListComponent(names)
+        ), 
+    document.getElementById('center')
+)
+
+const btn = document.createElement('button')
+btn.innerText = 'Click'
+
+
+Domworker.mount(new CenterComponent(btn), 
+document.getElementById('center_second')
+)
+
+const brush = {
+    createComponent: function() {console.log('Я создатель')},
+
+
+    render : function(canvas) {
+        canvas.style.width = '250px'
+        canvas.style.height = '250px'
+        canvas.style.backgroundColor = this.color
+    },
+
+    setColor: function(color) {
+        this.color = color
+        return this
+    }
+
+
+}
+
+Domworker.mount(
+    brush.setColor('red'),
+    document.getElementById('other')
+)
+
+
+
